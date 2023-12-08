b0fc564d53b6f20e00d3d92584b39fca56073d97
KeatonCable1 <keatoncable1@gmail.com>
2023-12-08 02:51:58 -0500

    assignment-3

--- Assignment3/src/com/coderscampus/login/UserLoginApplcation.java
+++ Assignment3/src/com/coderscampus/login/UserLoginApplcation.java
@@ -1,5 +1,38 @@
 package com.coderscampus.login;
 
-public class UserLoginApplcation {
+import java.util.List;
+import java.util.Scanner;
 
+public class UserLoginApplcation {
+	public static void main(String[] args) {
+		UserService userservice = new UserService();
+		List<User> users = userservice.readUserDataFromFile("data.txt");
+		
+		Scanner scanner = new Scanner(System.in);
+		int loginAttempts = 0;
+		boolean loggIn = false;
+		
+		while (loginAttempts < 5 && !loggIn) {
+			System.out.println("Enter your email: ");
+			String email = scanner.nextLine().toLowerCase();
+			
+			System.out.print("Enter your password: ");
+			String password = scanner.nextLine();
+			
+			for (User user : users) {
+				if (user.getUsername().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
+					System.out.println("Welcome: " + user.getName());
+					loggIn = true;
+					break;
+				}
+			}
+			if (!loggIn) {
+				System.out.println("Invalid login, please try again");
+				loginAttempts++;
+			}
+		}
+		if (!loggIn) {
+			System.out.println("Too many failed login attempts, you are now locked out.");
+		}
+	}
 }
--- Assignment3/src/com/coderscampus/login/UserService.java
+++ Assignment3/src/com/coderscampus/login/UserService.java
@@ -1,5 +1,37 @@
 package com.coderscampus.login;
 
-public class UserService {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class UserService {
+	public List<User> readUserDataFromFile(String FileName) {
+		List<User> users = new ArrayList<>();
+		
+		try {
+			File file = new File(FileName);
+			Scanner scanner = new Scanner(file);
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] userData = line.split(",");
+				
+				if (userData.length == 3) {
+					String username = userData[0];
+					String password = userData[1];
+					String name = userData[2];
+					
+					User user = new User(username, password, name);
+					users.add(user);
+				}
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.print("File not found: " + FileName);
+		}
+		
+		return users;
+	}
 }
