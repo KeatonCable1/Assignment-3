7f5c9bbf38fb79dd9f4aaf20f578b405de10e340
KeatonCable1 <keatoncable1@gmail.com>
2023-12-14 02:37:58 -0500

    Update UserLoginApplcation.java

--- Assignment3/src/com/coderscampus/login/UserLoginApplcation.java
+++ Assignment3/src/com/coderscampus/login/UserLoginApplcation.java
@@ -12,7 +12,7 @@ public class UserLoginApplcation {
 		int loginAttempts = 0;
 		boolean loggIn = false;
 		
-		while (loginAttempts < 4 && !loggIn) {
+		while (loginAttempts < 5 && !loggIn) {
 			System.out.println("Enter your email: ");
 			String email = scanner.nextLine().toLowerCase();
 			
@@ -25,7 +25,7 @@ public class UserLoginApplcation {
 					loggIn = true;
 					break;
 				}
-			}
+			} scanner.close();
 			if (!loggIn) {
 				System.out.println("Invalid login, please try again");
 				loginAttempts++;
